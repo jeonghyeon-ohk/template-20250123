5771138b2a39468fee6f20fbe43104e4d4bbe1dd
jeonghyeon-ohk <ok6737@naver.com>
2025-01-23 14:37:18 +0900

    get/post

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -2,6 +2,7 @@ package com.example.template.domain.post.post.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,7 +14,7 @@ public class PostController {
     @ResponseBody
     public String showWrite() { // 문서 처리하게 ~ 부서로 가세요
         return """
-                <form action="/posts/doWrite">
+                <form action="/posts/doWrite" method="post">
                   <input type="text" name="title" placeholder="제목" />
                   <textarea name="content"></textarea>
                   <input type="submit" value="등록" />
@@ -21,7 +22,7 @@ public class PostController {
                 """; // 문서작성해서 오세요
     }
 
-    @GetMapping("/doWrite")
+    @PostMapping("/doWrite")
     @ResponseBody
     public String doWrite(String title, String content) {
 
