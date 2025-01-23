fe8d60d2a943c31d21a75ef282fa7a46ce5cdb2f
jeonghyeon-ohk <ok6737@naver.com>
2025-01-23 14:36:31 +0900

    doWrite

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -2,12 +2,34 @@ package com.example.template.domain.post.post.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
+@RequestMapping("/posts")
 public class PostController {
-    @GetMapping("/")
+
+    @GetMapping("/write")
     @ResponseBody
-    public String hello() {
-        return "hello";
+    public String showWrite() { // 문서 처리하게 ~ 부서로 가세요
+        return """
+                <form action="/posts/doWrite">
+                  <input type="text" name="title" placeholder="제목" />
+                  <textarea name="content"></textarea>
+                  <input type="submit" value="등록" />
+                </form>
+                """; // 문서작성해서 오세요
     }
+
+    @GetMapping("/doWrite")
+    @ResponseBody
+    public String doWrite(String title, String content) {
+
+        return """
+                <h1>게시물 조회</h1>
+                <div>%s</div>
+                <div>%s</div>
+                """.formatted(title, content); // 문서 처리해드릴게요
+    }
+
 }
