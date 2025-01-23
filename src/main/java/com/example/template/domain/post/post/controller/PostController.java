c9442d42d1f705ff84bb4b27c51c58bdfbd5ded5
jeonghyeon-ohk <ok6737@naver.com>
2025-01-23 15:07:10 +0900

    title, content validation

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -1,10 +1,7 @@
 package com.example.template.domain.post.post.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/posts")
@@ -12,25 +9,33 @@ public class PostController {
 
     @GetMapping("/write")
     @ResponseBody
-    public String showWrite() { // 문서 처리하게 ~ 부서로 가세요
+    public String showWrite() {
         return """
-                <form action="/posts/doWrite" method="post">
+                <form method="post">
                   <input type="text" name="title" placeholder="제목" />
                   <textarea name="content"></textarea>
                   <input type="submit" value="등록" />
                 </form>
-                """; // 문서작성해서 오세요
+                """;
     }
 
-    @PostMapping("/doWrite")
+    @PostMapping("/write")
     @ResponseBody
     public String doWrite(String title, String content) {
+
+        if(title.isBlank() || title == null) {
+            return "제목을 입력해주세요";
+        }
+
+        if(content.isBlank() || content == null) {
+            return "내용을 입력해주세요";
+        }
 
         return """
                 <h1>게시물 조회</h1>
                 <div>%s</div>
                 <div>%s</div>
-                """.formatted(title, content); // 문서 처리해드릴게요
+                """.formatted(title, content);
     }
 
 }
